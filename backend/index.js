3ade37cf6103ee43fe16b383b49ce35bd12df7cc
unknown <dylan3pearson@gmail.com>
2025-10-10 14:18:11 +0200

    update

--- backend/index.js
+++ backend/index.js
@@ -15,12 +15,7 @@ import cartRoutes from './routes/cartRoutes.js';
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin: 'http://localhost:8080',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 
 //this is to test if the db is connected successfully
 app.get('/test-db', async (req, res) => {
